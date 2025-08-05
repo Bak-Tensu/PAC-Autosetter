36843bf9db0d34708283e0a279153f8e1482f2d7
Bak-Tensu <223912333+Bak-Tensu@users.noreply.github.com>
2025-08-05 23:18:39 +0200

    Updated logic for manual regeneration
    Also commented code for easier comprehension.

--- options.js
+++ options.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.getElementById("save").addEventListener("click", saveOptions);
+document.getElementById("generate-now").addEventListener("click", generatePACInOptions);
 
+// Retrieve config from storage, or assign defaults if not found
 async function restoreOptions() {
   const { desired_name, domains, auto_mode, last_updated } = await browser.storage.local.get([
     "desired_name",
@@ -17,6 +19,7 @@ async function restoreOptions() {
   document.getElementById("lastUpdated").textContent = last_updated || "Never";
 }
 
+// Store config in storage, then retrieve the status from Auto Mode and shortly display a short confirmation
 async function saveOptions() {
   const desired_name = document.getElementById("proxyName").value.trim();
   const domains = document.getElementById("domains").value.split("\n").map(d => d.trim()).filter(Boolean);
@@ -33,3 +36,28 @@ async function saveOptions() {
   document.getElementById("status").textContent = "Saved!";
   setTimeout(() => location.reload(), 800);
 }
+
+// Manually create a PAC URI
+async function generatePACInOptions() {
+  const statusDiv = document.getElementById("manualUpdateFeedback");
+  statusDiv.textContent = "Updating PAC...";
+  statusDiv.style.color = "black";
+  console.log("Manual update launched from options.")
+
+  try {
+    await browser.runtime.getBackgroundPage().then(bg => bg.updatePAC());
+    statusDiv.textContent = "PAC updated successfully!";
+    statusDiv.style.color = "green";
+  } catch (err) {
+    console.error("PAC update from options page failed:", err);
+    statusDiv.textContent = "PAC update failed. See console.";
+    statusDiv.style.color = "red";
+    alert("Failed to regenerate PAC. See console for details.");
+  }
+
+  // Fade out after 5 seconds then reload to update the page's state
+  setTimeout(() => {
+    statusDiv.textContent = "";
+    location.reload();
+  }, 5000);
+};
